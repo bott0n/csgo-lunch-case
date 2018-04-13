d52ba625e65fd6ee871e0c38ff7efcc10b4e87cd
jacky2001114 <jacky2001114@gmail.com>
2018-04-13 21:00:54 +0800

    update

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
 $(function(){
-    var right = 0;
+
     
-    var count=0;
 
     $('#btn').on('click',function(){
+        render()
+        var right = 0;
+    
+    var count=0;
+    var speed=50;
+        $('.overlay').css('display','none');
+        $('#btn').css('display','none');
      var timer = setInterval(function(){
-            right+=50
+         
+            right+=speed
             count++
+
             $('.slider-wrapper').css('right',right+'px')
-            if(count==300){
+            if(count>=150 ){
+                speed=30
+            }
+            if(count>=200){
+                speed=20
+            }
+            if(count>=250){
+                speed=10
+            }
+            if(count>=300){
+                speed=5
+            }
+            if(count>=320){
+                speed=4
+            }
+            if(count>=340){
+                speed=3
+            }
+            if(count>=360){
+                speed=2
+            }
+            if(count>=380){
+                speed=1
+            }
+            if(count>=450){
                 clearInterval(timer)
+            
+                $('#btn').css('display','block');
             }
         },20)
     })
